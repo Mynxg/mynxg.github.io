3a1b7ef891cc9a7d36cc388c4f8eb05e33daef02
mynxg <mynxg@qq.com>
2023-04-17 03:37:01 +0000

    Deploying to gh-pages from @ mynxg/vuepress-theme-hope-blog@14ec123a6f9e6c95655aa3c7a097b715ab91fd4d 🚀

---
+++ assets/nav.html-d997f170.js
@@ -0,0 +1,6 @@
+import{aP as c,Q as a,S as i,ae as n,E as t}from"./framework-1a3f7ddc.js";const r={};function s(p,l){const e=t("NavCard"),o=t("MyLinks");return a(),i("div",null,[n(e,{arr:`[{"title":"One Nav","url":"https://nav.lxip.top/","desc":"导航","icon":"/navicon/pdai.ico"},
+{"title":" Nginx Proxy Manager","url":"https://npm.lxip.top/","desc":"反向代理","icon":"/navicon/ruan.ico"},
+{"title":"单服务器数据检测","url":"https://watch.lxip.top/","desc":"单服务器数据检测","icon":"/navicon/r2coding.png"},
+{"title":"导航页","url":"https://navs.lxip.top/","desc":"导航","icon":"/navicon/yuque.png"},
+{"title":"兰空图床","url":"http://139.224.55.130:9080","desc":"方向代理有问题~~~","icon":"/navicon/dd.jpg"}
+]`}),n(o,{type:"mynav",src:"mynav"})])}const d=c(r,[["render",s],["__file","nav.html.vue"]]);export{d as default};
