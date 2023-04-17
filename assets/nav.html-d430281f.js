b309595cfba1088ee5e66f0ebf1dcea0f3493dee
mynxg <mynxg@qq.com>
2023-04-17 06:04:09 +0000

    Deploying to gh-pages from @ mynxg/vuepress-theme-hope-blog@daca263b1ed0d8bd8f4710ca14c39ba498075359 🚀

--- assets/nav.html-d430281f.js
+++ assets/nav.html-d430281f.js
@@ -1,6 +1,6 @@
-import{aP as c,Q as a,S as i,ae as n,E as t}from"./framework-1a3f7ddc.js";const r={};function s(p,l){const e=t("NavCard"),o=t("MyLinks");return a(),i("div",null,[n(e,{arr:`[{"title":"One Nav","url":"https://nav.lxip.top/","desc":"导航","icon":"/navicon/pdai.ico"},
+import{aP as t,Q as e,S as a,ab as o,ae as c,E as i}from"./framework-1a3f7ddc.js";const r={};function s(p,l){const n=i("MyLinks");return e(),a("div",null,[o(` <NavCard arr='[{"title":"One Nav","url":"https://nav.lxip.top/","desc":"导航","icon":"/navicon/pdai.ico"},
 {"title":" Nginx Proxy Manager","url":"https://npm.lxip.top/","desc":"反向代理","icon":"/navicon/ruan.ico"},
 {"title":"单服务器数据检测","url":"https://watch.lxip.top/","desc":"单服务器数据检测","icon":"/navicon/r2coding.png"},
 {"title":"导航页","url":"https://navs.lxip.top/","desc":"导航","icon":"/navicon/yuque.png"},
 {"title":"兰空图床","url":"http://139.224.55.130:9080","desc":"方向代理有问题~~~","icon":"/navicon/dd.jpg"}
-]`}),n(o,{type:"mynav",src:"mynav"})])}const d=c(r,[["render",s],["__file","nav.html.vue"]]);export{d as default};
+]'></NavCard> `),c(n,{type:"mynav",src:"mynav"})])}const v=t(r,[["render",s],["__file","nav.html.vue"]]);export{v as default};
