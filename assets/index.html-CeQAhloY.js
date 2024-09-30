6136188e89f6018d5226b7dca1e72bf504539fb0
mynxg <nxg0916@gmail.com>
2024-09-30 14:14:16 +0000

    Deploying to gh-pages from @ mynxg/vuepress-hope-blog@7ca7792f37f7bc45c62590a356f2954218aee223 🚀

---
+++ assets/index.html-CeQAhloY.js
@@ -0,0 +1,44 @@
+import{_ as c}from"./plugin-vue_export-helper-DlAUqK2U.js";import{r as s,c as r,d as t,a,b as e,n as i,g as o,o as p}from"./app-Id1QszRS.js";const g={},m=e("h2",{id:"介绍",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#介绍"},[e("span",null,"介绍")])],-1),d=e("blockquote",null,[e("p",null,"很荣幸与以下网站建立友情链接，让我们共同打造一个热爱编程、分享知识的网络社区吧！")],-1),h=e("h2",{id:"友链",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#友链"},[e("span",null,"友链")])],-1),b={class:"vp-card-container"};function u(y,k){const l=s("clock"),n=s("VPCard");return p(),r("div",null,[t(l),m,d,h,a(` <NavCard arr='[{"title":"blog","url":"https://hisnxg.gitee.io/","desc":"hexo搭建的博客模板","icon":"/navicon/dd.jpg"},
+{"title":"keney","url":"https://nnxx.me/","desc":"基于hexo-matery搭建的博客","icon":"https://pic.imgdb.cn/item/640d9bf1f144a010073218ac.png"},
+{"title":"Barry","url":"https://www.barryi.me/","desc":"未知全貌，不予置评","icon":"https://easyimage.smitten.top/i/2023/01/26/xh2qvw.jpg"}
+]'></NavCard> `),a(` 
+::: projects
+\`\`\`yaml
+
+- icon: https://cdn.lxip.top/gh/imnxg/img-post@main/blog/nn.svg
+  name: keney
+  desc: 基于hexo-matery搭建的博客
+  link: https://blog.nnxx.me/
+
+- icon: https://easyimage.smitten.top/i/2023/01/26/xh2qvw.jpg
+  name: barryi
+  desc: 未知全貌，不予置评
+  link: https://www.barryi.me/
+
+- icon: https://image.liubing.me/2023/02/11/50994f9710667.ico
+  name: blog
+  desc: hexo搭建的博客
+  link: https://mynxg.gitee.io/
+
+- icon: https://pic.imgdb.cn/item/640d9bf1f144a010073218ac.png
+  name: VuePress
+  desc: 编程笔记、记录bug
+  link: https://blogs.lxip.top
+
+# - icon: https://theme-hope.vuejs.press/logo.png
+#   name: VuePress Theme Hope
+#   desc: 本站博客所用主题
+#   link: https://theme-hope.vuejs.press/zh/
+
+# - icon: https://image.liubing.me/2023/02/11/a7643dcbbc234.png
+#   name: Buddy
+#   desc: 本站自动化打包构建发布工具。
+#   link: https://buddy.works
+
+# - icon: https://image.liubing.me/2023/02/05/834597e9e927e.png
+#   name: Waline
+#   desc: 本站评论所用服务。
+#   link: https://waline.js.org/
+\`\`\`
+
+::: `),e("div",b,[t(n,i(o({title:"barryi",desc:"未知全貌，不予置评",logo:"https://easyimage.smitten.top/i/2023/01/26/xh2qvw.jpg",link:"https://www.barryi.me/",background:"rgba(253, 230, 138, 0.15)"})),null,16),t(n,i(o({title:"VuePress",desc:"编程笔记、记录bug",logo:"https://pic.imgdb.cn/item/640d9bf1f144a010073218ac.png",link:"https://blogs.lxip.top",background:"rgba(253, 230, 138, 0.15)"})),null,16),t(n,i(o({title:"blog",desc:"hexo搭建的博客",logo:"https://image.liubing.me/2023/02/11/50994f9710667.ico",link:"https://mynxg.gitee.io/",background:"rgba(253, 230, 138, 0.15)"})),null,16),t(n,i(o({title:"Waline",desc:"本站评论所用服务。",logo:"https://image.liubing.me/2023/02/05/834597e9e927e.png",link:"https://waline.js.org/",background:"rgba(253, 230, 138, 0.15)"})),null,16)])])}const f=c(g,[["render",u],["__file","index.html.vue"]]),v=JSON.parse('{"path":"/links/","title":"友链","lang":"zh-CN","frontmatter":{"title":"友链","icon":"link","category":["友链"],"tag":["博客链接"],"sticky":false,"star":false,"article":false,"description":"介绍 很荣幸与以下网站建立友情链接，让我们共同打造一个热爱编程、分享知识的网络社区吧！ 友链","gitInclude":[],"head":[["meta",{"property":"og:url","content":"https://blog.lxip.top/links/"}],["meta",{"property":"og:site_name","content":"所遇所思"}],["meta",{"property":"og:title","content":"友链"}],["meta",{"property":"og:description","content":"介绍 很荣幸与以下网站建立友情链接，让我们共同打造一个热爱编程、分享知识的网络社区吧！ 友链"}],["meta",{"property":"og:type","content":"website"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"article:author","content":"Mr.keney"}],["meta",{"property":"article:tag","content":"博客链接"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"WebPage\\",\\"name\\":\\"友链\\",\\"description\\":\\"介绍 很荣幸与以下网站建立友情链接，让我们共同打造一个热爱编程、分享知识的网络社区吧！ 友链\\"}"]]},"headers":[{"level":2,"title":"介绍","slug":"介绍","link":"#介绍","children":[]},{"level":2,"title":"友链","slug":"友链","link":"#友链","children":[]}],"readingTime":{"minutes":1.5,"words":450},"filePathRelative":"links/README.md","excerpt":"","autoDesc":true}');export{f as comp,v as data};
