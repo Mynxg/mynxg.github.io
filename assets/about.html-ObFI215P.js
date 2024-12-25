9ff8343cd7b268336ba8b9f630e5337d7359c138
mynxg <nxg0916@gmail.com>
2024-12-25 10:44:10 +0000

    Deploying to gh-pages from @ mynxg/vuepress-hope-blog@8e4b8151de2b15ddf08411e8429f8e252e84f34f 🚀

---
+++ assets/about.html-ObFI215P.js
@@ -0,0 +1,45 @@
+import{_ as a}from"./plugin-vue_export-helper-DlAUqK2U.js";import{c as r,a as n,b as i,d as o,n as s,g as l,r as g,o as p}from"./app-WwZNQFr4.js";const c={};function m(d,e){const t=g("VPCard");return p(),r("div",null,[e[0]||(e[0]=n("h1",{id:"关于本站",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#关于本站"},[n("span",null,"关于本站")])],-1)),e[1]||(e[1]=n("p",null,"一个基于 VuePress theme hope 的个人博客，主要记录日常开发问题和一些代码笔记。",-1)),e[2]||(e[2]=n("h2",{id:"服务提供",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#服务提供"},[n("span",null,"服务提供")])],-1)),e[3]||(e[3]=n("p",null,"本站由以下内容提供服务",-1)),i(` ::: projects
+\`\`\`yaml
+
+- icon: https://image.liubing.me/2023/02/11/7f17f3cf426c3.png
+  name: Cloudflare
+  desc: 提供免费的 CDN 服务。
+  link: https://www.cloudflare.com/
+
+# - icon: https://image.liubing.me/2023/02/11/a6de6381e4ae6.png
+#   name: Contabo
+#   desc: VPS 所属供应商。
+#   link: https://contabo.com/
+
+# - icon: https://image.liubing.me/2023/02/11/50994f9710667.ico
+#   name: 宝塔 Linux 面板
+#   desc: 用来管理 VPS
+#   link: https://cn.vuejs.org/
+
+- icon: https://vuepress.vuejs.org/hero.png
+  name: VuePress
+  desc: 博客驱动引擎。
+  link: https://vuepress.vuejs.org/zh/
+
+- icon: https://theme-hope.vuejs.press/logo.png
+  name: VuePress Theme Hope
+  desc: 本站博客所用主题
+  link: https://theme-hope.vuejs.press/zh/
+
+- icon: https://image.liubing.me/2023/02/11/a7643dcbbc234.png
+  name: Buddy
+  desc: 本站自动化打包构建发布工具。
+  link: https://buddy.works
+
+- icon: https://image.liubing.me/2023/02/05/834597e9e927e.png
+  name: Waline
+  desc: 本站评论所用服务。
+  link: https://waline.js.org/
+
+\`\`\`
+::: `),o(t,s(l({title:"Cloudflare",desc:"提供免费的 CDN 服务",logo:"https://image.liubing.me/2023/02/11/7f17f3cf426c3.png",link:"https://www.cloudflare.com/",background:"rgb(223, 242, 235)"})),null,16),o(t,s(l({title:"netlify",desc:"部署服务商",logo:"https://www.netlify.com/assets/logos/full/small/lightmode/logo-netlify-small-monochrome-lightmode.svg",link:"https://www.netlify.com/",background:"rgb(185, 229, 232)"})),null,16),o(t,s(l({title:"Baidu Analytics",desc:"百度分析",logo:"https://tongji.baidu.com/web5/image/logo.png",link:"https://tongji.baidu.com/",background:"rgb(122, 178, 211)"})),null,16),o(t,s(l({title:"51La Analytics",desc:"51La分析",logo:"https://v6.51.la/img/logo.7ed53b91.png",link:"https://v6.51.la/",background:"rgb(74, 98, 138)"})),null,16),o(t,s(l({title:"google-analytics",desc:"google分析",logo:"https://cdn.jsdelivr.net/gh/imnxg/imgbeds/icon/googlelogo_color_272x92dp.png",link:"https://www.cloudflare.com/",background:"rgb(191, 236, 255)"})),null,16),o(t,s(l({title:"VuePress Theme Hope",desc:"本站博客所用主题",logo:"https://theme-hope.vuejs.press/logo.png",link:"https://theme-hope.vuejs.press/zh/",background:"rgb(205, 193, 255)"})),null,16),o(t,s(l({title:"giscus-discussions",desc:"评论所用服务",logo:"https://cdn.jsdelivr.net/gh/imnxg/imgbeds/icon/githubIcon.svg",link:"https://github.com/mynxg/giscus-discussions/discussions",background:"rgb(255, 204, 234)"})),null,16),i(" cdn https://cdn.lxip.top/gh/imnxg/img-post@main/blog/nn.svg  "),i(` ## 博客
+<MyLinks type="friend" src="friend"/> `),i(` ## 仓库连接
+
+本站所有内容及代码均开源，可通过下面 👇 的链接访问
+
+[keney仓库](https://github.com/mynxg/) `)])}const b=a(c,[["render",m],["__file","about.html.vue"]]),f=JSON.parse('{"path":"/blog/about.html","title":"关于本站","lang":"zh-CN","frontmatter":{"toc":false,"breadcrumb":false,"gitInclude":[],"description":"关于本站 一个基于 VuePress theme hope 的个人博客，主要记录日常开发问题和一些代码笔记。 服务提供 本站由以下内容提供服务","head":[["meta",{"property":"og:url","content":"https://blog.lxip.top/blog/about.html"}],["meta",{"property":"og:site_name","content":"所遇所思"}],["meta",{"property":"og:title","content":"关于本站"}],["meta",{"property":"og:description","content":"关于本站 一个基于 VuePress theme hope 的个人博客，主要记录日常开发问题和一些代码笔记。 服务提供 本站由以下内容提供服务"}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"zh-CN"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"关于本站\\",\\"image\\":[\\"\\"],\\"dateModified\\":null,\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"Mr.keney\\",\\"url\\":\\"https://github.com/mynxg\\"}]}"]]},"headers":[{"level":2,"title":"服务提供","slug":"服务提供","link":"#服务提供","children":[]}],"readingTime":{"minutes":1.28,"words":384},"filePathRelative":"blog/about.md","excerpt":"","autoDesc":true}');export{b as comp,f as data};
